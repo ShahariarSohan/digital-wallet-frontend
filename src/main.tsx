e26f9f938d9477c7fd29ca02a20dc9177c8db590
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-08-21 22:20:10 +0600

    primary navbar added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './redux/store.ts'
+import { ThemeProvider } from './providers/ThemeProvide.tsx'
+
+import { RouterProvider } from 'react-router'
+import router from './routes/index.tsx'
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ReduxProvider store={store}>
+      <ThemeProvider
+        defaultTheme="system"
+        storageKey="vite-ui-theme"
+      ><RouterProvider router={router}></RouterProvider></ThemeProvider>
+    </ReduxProvider>
+  </StrictMode>
+);
